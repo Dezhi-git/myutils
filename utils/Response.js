bda0a92ede63272413943ec24f6d82e03f5a0438
Dezhi-git <17688924369@163.com>
2018-04-19 14:34:22 +0800

    delete class sync

--- utils/Response.js
+++ utils/Response.js
@@ -1,10 +1,4 @@
-// class Response{
-//     constructor(code, msg, data){
-//         this.code = code || null;
-//         this.msg = msg || "";
-//         this.data = data || null;
-//     }
-// }
+
 function  Response(code, msg, data) {
     this.code = code || null;
     this.msg = msg || "";
